9ee12d4e2cc5671ba57584c7181ef14ee633d8b4
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2024-09-15 02:42:24 +0200

    added documentation to TickCounter

--- DownloaderNET/TickCounter.cs
+++ DownloaderNET/TickCounter.cs
@@ -4,6 +4,13 @@ class TickCounter : ITickCounter
     {
     }
 
+
+
+    /// <summary>
+    /// Retrieves the number of milliseconds that have elapsed since the system was started, up 
+    /// to 24.9 days. After 24.9 days this will start again from 0.
+    /// </summary>
+    /// <returns>milliseconds since system start</returns>
     int ITickCounter.GetTickCount()
     {
         return Environment.TickCount & int.MaxValue;
